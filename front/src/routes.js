f895862f6c8530ab83d4ad951dcdd134130b5088
Ana204 <60896884+Ana204@users.noreply.github.com>
2020-11-26 14:34:59 -0300

    subindo minha rota de historico

--- front/src/routes.js
+++ front/src/routes.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
-
+import Header from './components/Header'
+import TabelaDeHistorico from './components/TabelaDeHistorico'
+import Button from './components/Button'
+import NavLateral from './components/Nav_Lateral'
+import Logo from './components/Logo'
 import Login from './pages/login'
+
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Login}/>
+            <Route  path="/historico" component={TabelaDeHistorico} />
         </Switch>
     </BrowserRouter>
 )
