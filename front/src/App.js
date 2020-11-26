ed20d01ba7b8452c69b6b28923d58c7ce5d12bd0
Denison Portela <denisondealmeidaprtela@hotmail.com>
2020-11-25 22:36:49 -0300

    enviando global styles

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Login  from './pages/login'
+import NavLateral from './components/Nav_Lateral'
+import GlobalStyles from './styles/GlobalStyles'
 
 function App() {
   return (
     <div className="App">
-      <Login />
+  
+
+      <GlobalStyles />
     </div>
   );
 }
---
+++ front/src/components/Nav_Lateral/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 15vw;
+  height:100%;
+  position: fixed;
+  background-color: var(--color-azul);
+`;
+
+
