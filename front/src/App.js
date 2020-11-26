826b0e20207a817e3d6e996d9167743517ebb98c
Ana204 <60896884+Ana204@users.noreply.github.com>
2020-11-26 02:36:12 -0300

    Subindo a pasta de historico e o boostrap

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login  from './pages/login'
 import Header from './components/Header'
 import NavLateral from './components/Nav_Lateral'
 import GlobalStyles from './styles/GlobalStyles'
 import Button from './components/Button'
+import TabelaDeHistorico from './components/TabelaDeHistorico'
 
 function App() {
   return (
     <div className="App">
       {/* <Header title = ""/>  */}
       {/* <Button width = "" height = "" title = ""/> */}
+      <TabelaDeHistorico />
+
       {/*Carregando estilo global*/}
       <GlobalStyles />
     </div>
