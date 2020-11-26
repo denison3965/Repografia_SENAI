01cb801ced362faac928e235e564e0e064afd1e4
Denison Portela <denisondealmeidaprtela@hotmail.com>
2020-11-25 23:19:48 -0300

    Subindo tudo com a imagen da logo aparecendo

--- front/src/components/Logo/index.jsx
+++ front/src/components/Logo/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container, LogoSenai, Titulo } from './styles';
+import SENAI from '../../assets/img/senaiLogo.png'
 
 function Logo() {
   return (
       <Container>
+          <LogoSenai src={SENAI}/>
           <Titulo>Senai suico</Titulo>
           
       </Container>
