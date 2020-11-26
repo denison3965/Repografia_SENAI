2ec41d810186b19566615c159bb4e1d83d9af873
Ana204 <60896884+Ana204@users.noreply.github.com>
2020-11-26 16:07:43 -0300

    subindo tabeladeHistorico

--- front/src/routes.js
+++ front/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Header from './components/Header'
-import TabelaDeHistorico from './components/TabelaDeHistorico'
+import TabelaDeHistorico from './pages/TabelaDeHistorico'
 import Button from './components/Button'
 import NavLateral from './components/Nav_Lateral'
 import Logo from './components/Logo'
