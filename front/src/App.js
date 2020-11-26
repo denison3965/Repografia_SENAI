819ab55fcbf564d06a45879385f4adb97a67e1f1
Ana204 <60896884+Ana204@users.noreply.github.com>
2020-11-25 22:39:35 -0300

    Subindo a pasta header

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Login  from './pages/login'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Header />
     </div>
   );
 }
