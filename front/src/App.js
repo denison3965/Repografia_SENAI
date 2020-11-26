297ae13f9b2f313740129d4e636c04ea4def7031
obragaa <f.braga.duarte02@gmail.com>
2020-11-26 15:24:18 -0300

    Importando bootstrap

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,13 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Login  from './pages/login'
-import Header from './components/Header'
-import NavLateral from './components/Nav_Lateral'
 import GlobalStyles from './styles/GlobalStyles'
-<<<<<<< HEAD
-import Formulario from './components/formulario'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
-=======
+
 import Routes from './routes'
->>>>>>> 6021ea00bfb3d45121fc0bff7746c28a3a6e12ab
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Formulario/>
-=======
->>>>>>> 6021ea00bfb3d45121fc0bff7746c28a3a6e12ab
 
       <Routes />
       <GlobalStyles />
--- front/src/routes.js
+++ front/src/routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import Login  from './pages/login'
+import Header from './components/Header'
+import NavLateral from './components/Nav_Lateral'
+import Formulario from './components/formulario'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
-
-import Login from './pages/login'
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Login}/>
+            <Route path="/Formulario" component={Formulario}/>
         </Switch>
     </BrowserRouter>
 )
