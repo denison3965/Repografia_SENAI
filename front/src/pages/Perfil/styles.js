7e215e35c59854897b1f6faafbecf55a4d242e78
firgo12 <60897048+firgo12@users.noreply.github.com>
2020-11-27 17:33:11 -0300

    Perfil

---
+++ front/src/pages/Perfil/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #FFFFFF;
+  hr{
+      background-color: #c11b04;
+  }
+  .sair{
+    position: relative;
+    margin-top: 4px;
+    left: 90vw;
+    width: 5vw;
+    color: #666666;
+    background-color: #FFFFFF;
+    text-align: center;
+    font-size: 16px;
+    cursor: pointer;
+  }
+`;
--- front/src/routes.js
+++ front/src/routes.js
@@ -3,6 +3,7 @@ import Login  from './pages/login'
 import Formulario from './components/formulario'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Adm_Registros from './pages/Adm_Registros'
+import Perfil from './pages/Perfil'
 import Historico from './pages/Perfil_Historico'
 import Graficos from './components/Graficos'
 import Detalhes from './pages/Detalhes'
@@ -15,6 +16,7 @@ const Routes = () => (
             <Route path="/Historico" component={Historico} />
             <Route path="/Formulario" component={Formulario}/>
             <Route path="/graficos" component={Graficos}/>
+            <Route path="/Perfil" component={Perfil}/>
             <Route path="/detalhes" render={(props) => <Detalhes {...props} data=""/>}/>
         </Switch>
     </BrowserRouter>
