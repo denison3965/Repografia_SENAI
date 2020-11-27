4671261d5af2dcbe8570799b6cd5614d0d502c9e
Ana204 <60896884+Ana204@users.noreply.github.com>
2020-11-27 02:33:40 -0300

    Subindo componente do grafico

--- front/src/routes.js
+++ front/src/routes.js
@@ -4,6 +4,7 @@ import Formulario from './components/formulario'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import Adm_Registros from './pages/Adm_Registros'
 import Historico from './pages/Perfil_Historico'
+import Graficos from './components/Graficos'
 
 const Routes = () => (
     <BrowserRouter>
@@ -12,6 +13,7 @@ const Routes = () => (
             <Route path="/adm/registros" component={Adm_Registros} />
             <Route path="/Historico" component={Historico} />
             <Route path="/Formulario" component={Formulario}/>
+            <Route path="/graficos" component={Graficos}/>
         </Switch>
     </BrowserRouter>
 )
