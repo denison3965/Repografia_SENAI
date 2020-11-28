7e5e366e0f7a9bc1876c1033fbec15fa2463a4e4
firgo12 <60897048+firgo12@users.noreply.github.com>
2020-11-28 15:08:32 -0300

    Perfil

--- front/src/pages/Perfil/index.jsx
+++ front/src/pages/Perfil/index.jsx
@@ -9,10 +9,34 @@ function perfil() {
           <Header/>
 
           <div className="sair">
-              <div>Sair</div>
+              < div>Sair</div>
+          </div>
+
+          <div className="localizacao">
+                <div className="seu_perfil">Seu Perfil</div>
+                <div className="seu_historico">Seu Histórico</div>
           </div>
          
           <hr/>
+
+          <div className="informacoes">
+                <div className="telefone">Telefone:</div>
+                <div className="numero">Número:</div>
+                <div className="nif">NIF:</div>
+          </div>
+
+          <div className="informacao">
+                    <div className="email">e-mail:</div>
+                    <div className="cargo">Cargo:</div>
+          </div>
+
+          <div className="senha_posicao">
+                <div>Editar Senha</div>
+          </div>
+
+          <div>
+                <div className="posicao_requisicao">Nova Requisição</div>
+          </div>
       </Container>
   ) 
 }
