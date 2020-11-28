7bd0c3f5af907b21cf612cba296f92859cf29e16
ana204 <anagomes.lucia@hotmail.com>
2020-11-28 02:56:55 -0300

    Subindo a tela dos graficos e relatorios

--- front/src/components/Graficos/index.jsx
+++ front/src/components/Graficos/index.jsx
@@ -45,7 +45,6 @@ function Graficos() {
             'Milton',
             'Sangra',
             'Claudia',
-            'teste',
         ],
         datasets:[
             {
--- front/src/pages/Mesa_Grafico/styles.js
+++ front/src/pages/Mesa_Grafico/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+ 
 `;
 
 export const MenuLateral = styled.div`
@@ -11,6 +11,51 @@ export const MenuLateral = styled.div`
 export const InfoBox = styled.div`
    width: 88vw;
    min-height: 100vh;
-   margin-left: 200px;
+   margin-left: 225px;
+   margin-top: 20px;
    position: absolute;
 `;
+
+export const Navegation = styled.nav`
+  height:30px;
+  width: 77vw;
+  border-radius: 5px;
+  margin-left:30px;
+  background-color: #D5ECF2;
+  margin-bottom: 15px;
+  padding-top: 4px;
+      
+    ul{
+      list-style: none;
+      margin-left: 15px;
+        li{
+          color: #666;
+        }
+    }
+`;
+
+export const Info = styled.div`
+`;
+
+export const Title = styled.div`
+   font-size: 30px;
+   width: 30vw;
+   margin-left: 30px;
+   color: black;
+`;
+export const Grafico = styled.div`
+   margin-top: 30px;
+`;
+export const Tabela = styled.div`
+   margin-top: 150px;
+   display:flex;
+   justify-content: end; 
+   margin-right: 100px;
+
+   /* @media screen and(max-width:3000px){
+      & {
+        margin-right: 200px;
+      }
+   } */
+`;
+
