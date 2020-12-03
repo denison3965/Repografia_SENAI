98ab5b96ae7496e6e6042a8003c3078bd468c51c
Denison Portela <denisondealmeidaprtela@hotmail.com>
2020-12-03 16:55:23 -0300

    jh

--- front/src/auth.js
+++ front/src/auth.js
@@ -3,24 +3,42 @@ import axios from 'axios'
 
 export const isAuthenticated = async() => {
     
-    let resultado
+
+    let retorno = false
 
     await axios.get("http://localhost:3000/v1/teste",  {
         method: 'GET',
-        headers: {'X-access-token': 'eyJhbGciOJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjA2OTYxNTQ3LCJleHAiOjE2MDY5NjE4NDd9.RMxSXze0ZdtReOeiWmBVcfeuwEiA7R--BjGyvpwHVmk'}
+        headers: {'X-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjA3MDIzNzA3LCJleHAiOjE2MDcwMjQwMDd9.5PNTUUEg_m98eB21GBQdy0nQSxHRxidIcaKzh4RakII'}
     })
-    .then ( async(res) => {
+    .then ((res) => {
+
+        return res
+
+    }).then((res) => {
 
         console.log(res.data[0].auth)
-        resultado = await res.data[0].auth
-
+         
+        if(res.data[0].auth) {
+            console.log("aqui e true")
+            retorno = true
+        }else{
+            console.log("aqui e false")
+            retorno = false
+        }
 
     }) .catch((err) => {
 
-        
+        console.log('token expirado ou token errado')
+        return false
     })
 
-    console.log(resultado)
+
+    console.log(`o retorno esta com ${retorno}`)
+    return retorno
+ 
+
+ 
+
 }
 
 // export const isAuthenticated = () => false
--- front/src/routes.js
+++ front/src/routes.js
@@ -12,19 +12,38 @@ import Funcionarios from './pages/funcionarios'
 import Cadastro from './pages/Cadastro'
 import PerfilAdm from './pages/Perfil_Adm'
 import HistoricoAdm from './pages/Historico_Adm'
-import { Component } from 'react';
 import { isAuthenticated } from "./auth";
 
 const PrivateRoute = ({ component: Component, ... rest}) => (
+
+
     <Route
     {...rest}
     render={props =>
-      isAuthenticated() == true ? (
-        <Component {...props} />
+
+      
+
+      {
+        let teste
+        // console.log(isAuthenticated())
         
-      ) : (
-        <Redirect to={{ pathname: "/", state: { from: props.location } }} />
-      )
+         isAuthenticated().then((res) =>{
+           console.log("Meu testtttttttte " + res)
+          res ? (
+            <Component {...props} />
+        
+          ) : (
+            <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+          )
+         } )
+        
+      }
+      // isAuthenticated() == true ? (
+      //   <Component {...props} />
+        
+      // ) : (
+      //   <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+      // )
     }
   />
 );
