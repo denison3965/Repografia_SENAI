5ba809f8fab24a7b9399f039d384b997dd46e98f
ana204 <anagomes.lucia@hotmail.com>
2021-03-03 18:14:28 -0300

    console.log auth.js

--- front/src/auth.js
+++ front/src/auth.js
@@ -19,21 +19,15 @@ export const isAuthenticated = async() => {
         console.log(res.data[0].auth)
          
         if(res.data[0].auth) {
-            console.log("aqui e true")
             retorno = true
         }else{
-            console.log("aqui e false")
             retorno = false
         }
 
     }) .catch((err) => {
-
-        console.log('token expirado ou token errado')
         return false
     })
 
-
-    console.log(`o retorno esta com ${retorno}`)
     return retorno
  
 
